2fa7addf7e51c5c2ccaf06817c1cc47ad1d9b6de
IF <139582705+infiniteflower@users.noreply.github.com>
2026-05-06 09:57:13 -0400

    feat: add high rate alert modal

--- app/components/UI/Bridge/routes.tsx
+++ app/components/UI/Bridge/routes.tsx
@@ -16,6 +16,7 @@ import { PriceImpactModal } from './components/PriceImpactModal';
 import { clearStackNavigatorOptions } from '../../../constants/navigation/clearStackNavigatorOptions';
 import { TokenWarningModal } from './components/TokenWarningModal';
 import { MissingPriceModal } from './components/MissingPriceModal';
+import { HighRateAlertModal } from './components/HighRateAlertModal';
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 type ScreenComponent = React.ComponentType<any>;
@@ -97,6 +98,10 @@ export const BridgeModalStack = () => (
     <ModalStack.Screen
       name={Routes.BRIDGE.MODALS.TOKEN_WARNING_MODAL}
       component={TokenWarningModal}
+    />
+    <ModalStack.Screen
+      name={Routes.BRIDGE.MODALS.HIGH_RATE_ALERT_MODAL}
+      component={HighRateAlertModal}
     />
   </ModalStack.Navigator>
 );
