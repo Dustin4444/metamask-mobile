5e2a930f499afca52a1c68acf585fc5a6e852bd5
IF <139582705+infiniteflower@users.noreply.github.com>
2026-05-07 16:01:32 -0400

    feat: first pass at loading state of batch sell review screen

--- app/components/UI/Bridge/utils/index.ts
+++ app/components/UI/Bridge/utils/index.ts
@@ -8,6 +8,7 @@ import {
 } from '@metamask/bridge-controller';
 import { ImageSourcePropType } from 'react-native';
 import imageIcons from '../../../../images/image-icons';
+import parseAmount from '../../../../util/parseAmount';
 
 export const isBridgeAllowed = (chainId: Hex | CaipChainId | string) => {
   if (!AppConstants.BRIDGE.ACTIVE) {
@@ -77,3 +78,17 @@ export const getTokenImageSource = (
 
   return undefined;
 };
+
+export function formatTokenBalance(balance: string): string {
+  const numericBalance = Number(balance);
+
+  if (numericBalance === 0) {
+    return '0';
+  }
+
+  if (numericBalance < 0.00001) {
+    return '< 0.00001';
+  }
+
+  return parseAmount(balance, 5) || balance;
+}
