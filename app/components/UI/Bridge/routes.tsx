fbb9d0c03b46e88bdcb663be65760d3f33d05965
IF <139582705+infiniteflower@users.noreply.github.com>
2026-05-08 10:57:44 -0400

    feat: add BatchSellDestinationTokenSelectorModal and integrate with BatchSellReview

--- app/components/UI/Bridge/routes.tsx
+++ app/components/UI/Bridge/routes.tsx
@@ -18,6 +18,7 @@ import { clearStackNavigatorOptions } from '../../../constants/navigation/clearS
 import { TokenWarningModal } from './components/TokenWarningModal';
 import { MissingPriceModal } from './components/MissingPriceModal';
 import { HighRateAlertModal } from './components/HighRateAlertModal';
+import { BatchSellDestinationTokenSelectorModal } from './components/BatchSellDestinationTokenSelectorModal';
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 type ScreenComponent = React.ComponentType<any>;
@@ -108,6 +109,10 @@ export const BridgeModalStack = () => (
     <ModalStack.Screen
       name={Routes.BRIDGE.MODALS.HIGH_RATE_ALERT_MODAL}
       component={HighRateAlertModal}
+    />
+    <ModalStack.Screen
+      name={Routes.BRIDGE.MODALS.BATCH_SELL_DESTINATION_TOKEN_SELECTOR_MODAL}
+      component={BatchSellDestinationTokenSelectorModal}
     />
   </ModalStack.Navigator>
 );
